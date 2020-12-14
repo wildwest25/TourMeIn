4cfdc1f66e6c4922c61364b442ae605ef23804b7
Matija Hamer <mhamer@student.unipu.hr>
2020-12-14 11:12:07 +0100

    Dodatak Login & Register page kostura i Bootstrap

--- tourmein/src/views/Home.vue
+++ tourmein/src/views/Home.vue
@@ -7,7 +7,9 @@
 
 <script>
 // @ is an alias to /src
+
 import HelloWorld from '@/components/HelloWorld.vue'
+//import Login from '@/components/Login.vue'
 
 export default {
   name: 'Home',
---
+++ tourmein/src/views/Register.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="register">
+    <h1>Registration Form</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="exampleUsername">Username</label>
+              <input
+                type="username"
+                class="form-control"
+                id="exampleUsername"
+                placeholder="Enter username"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleName">Name</label>
+              <input
+                type="name"
+                class="form-control"
+                id="exampleName"
+                placeholder="Enter name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleSurename">Last Name</label>
+              <input
+                type="lastname"
+                class="form-control"
+                id="exampleLastName"
+                placeholder="Enter Last Name"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
