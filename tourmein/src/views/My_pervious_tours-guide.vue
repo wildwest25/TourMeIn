7ada8dfb6dc3ae7859e544ebe0a83d74f60e4ec6
Matija Hamer <mhamer@student.unipu.hr>
2021-01-03 13:33:02 +0100

    Dodani elementi na stranici registracije

--- tourmein/src/views/My_pervious_tours-guide.vue
+++ tourmein/src/views/My_pervious_tours-guide.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="my_pervious_tours_guide">
-    <h1>My previous tours:</h1>
+  <div class="myPreviousToursGuide">
+    <h1>My previous tours</h1>
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-lg-6">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- tourmein/src/views/Register.vue
+++ tourmein/src/views/Register.vue
@@ -2,9 +2,8 @@
   <div class="register">
     <h1>Registration Form</h1>
     <div class="container">
-      <div class="row">
-        <div class="col-sm"></div>
-        <div class="col-sm">
+      <div class="row justify-content-md-center">
+        <div class="col-lg-6">
           <form>
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
@@ -55,10 +54,169 @@
                 placeholder="Enter Last Name"
               />
             </div>
+            <form
+              name="someForm"
+              method="post"
+              action="/someAction.do"
+              class="form-inline"
+            >
+              <div class="input-group">
+                <div class="form-group">
+                  <label for="gender"> Gender: </label>
+                  <div>
+                    <input
+                      type="radio"
+                      name="gen"
+                      id="male"
+                      style="width: 20px"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="male"> Male </label>
+                  <div>
+                    <input
+                      type="radio"
+                      name="gen"
+                      id="female"
+                      style="width: 20px"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="female"> Female</label>
+                  <div>
+                    <input
+                      type="radio"
+                      name="gen"
+                      id="other"
+                      style="width: 20px"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="other"> Other </label>
+                </div>
+              </div>
+            </form>
+            <form
+              name="someForm"
+              method="post"
+              action="/someAction.do"
+              class="form-inline"
+            >
+              <div class="input-group">
+                <div class="form-group">
+                  <label for="costPerHour"> Date of Birth: </label>
+                  <div class="form-group">
+                    <div class="form-group">
+                      <select class="form-control" id="day">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="form-group">
+                    <select class="form-control" id="month">
+                      <option>Jan</option>
+                      <option>Feb</option>
+                      <option>Mar</option>
+                      <option>Apr</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="form-group">
+                    <select class="form-control" id="year">
+                      <option>1950</option>
+                      <option>1951</option>
+                      <option>1952</option>
+                      <option>1953</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <form
+              name="someForm"
+              method="post"
+              action="/someAction.do"
+              class="form-inline"
+            >
+              <div class="form-group">
+                <label for="costPerHour"> Country: </label>
+                <div class="form-group">
+                  <div class="form-group">
+                    <select class="form-control" id="country">
+                      <option>Croatia</option>
+                      <option>Germany</option>
+                      <option>Italy</option>
+                      <option>Slovenia</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <form
+              name="someForm"
+              method="post"
+              action="/someAction.do"
+              class="form-inline"
+            >
+              <div class="form-group">
+                <label for="registerAs"> Register as: </label>
+                <div class="form-group">
+                  <div class="form-group">
+                    <select class="form-control" id="registerAs">
+                      <option>Tourist</option>
+                      <option>Guide</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </form>
+             <div class="form-group">
+              <label for="exampleSurename">City:</label>
+              <input
+                type="city"
+                class="form-control"
+                id="exampleCity"
+                placeholder="City"
+              />
+            </div>
+            <form
+              name="someForm"
+              method="post"
+              action="/someAction.do"
+              class="form-inline"
+            >
+              <div class="input-group">
+                <div class="form-group">
+                  <div>
+                    <input
+                      type="checkbox"
+                      name="agree"
+                      id="agree"
+                      style="width: 20px"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="perHour"> I have read and agree with Terms and Conditions Agreement </label>
+                </div>
+            </form>
+            
             <button type="submit" class="btn btn-primary">Register</button>
           </form>
         </div>
-        <div class="col-sm"></div>
       </div>
     </div>
   </div>
