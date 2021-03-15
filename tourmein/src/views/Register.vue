fd942cdfb5190b0879862f1887b3c879ec5d6ff6
Luka <wildwest25@hotmail.com>
2021-03-15 18:42:52 +0100

    Dodana email verifikacija registriranog korisnika

--- tourmein/src/views/Register.vue
+++ tourmein/src/views/Register.vue
@@ -236,7 +236,19 @@ export default {
 				.createUserWithEmailAndPassword(this.email, this.password)
 				.then(function() {
 					console.log('Uspjesna registracija');
-				})
+				
+				var user = firebase.auth().currentUser;
+
+                    user
+                        .sendEmailVerification()
+                        .then(function() {
+                            // Email sent.
+                        })
+                        .catch(function(error) {
+                            // An error happened.
+						});
+						})
+	
 				.catch(function(error) {
 					console.error('Doslo je do greske, error');
 				});
