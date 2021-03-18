b676d424d9fd3cf298a1543f5a4ef03d277b308e
TheBadPandaMan <51059974+TheBadPandaMan@users.noreply.github.com>
2021-03-18 18:55:45 +0100

    Update User_page.vue

---
+++ tourmein/src/views/User_page.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="user_page">
+        <div class="search-bar">
+                <b-form-input
+                @input="sarch_text()"
+                v-model="sarch.text"
+                type="text"
+                placeholder="Sarch Guides"
+                ></b-form-input>
+                <span class="sarch-icon">
+                    <i class="fas fa-search"></i>
+                </span>
+        </div>
+    </div>
+</template>
