af092808abd1150e0e7716661966fa33dcde8f4b
TheBadPandaMan <51059974+TheBadPandaMan@users.noreply.github.com>
2021-03-18 19:08:57 +0100

    Update User_page.vue

--- tourmein/src/views/User_page.vue
+++ tourmein/src/views/User_page.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="user_page">
-        <div class="search-bar">
-                <b-form-input
-                @input="sarch_text()"
-                v-model="sarch.text"
-                type="text"
-                placeholder="Sarch Guides"
-                ></b-form-input>
-                <span class="sarch-icon">
-                    <i class="fas fa-search"></i>
-                </span>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="input-group">
+                        <div class="form-outline">
+                            <input id="search-focus" type="search" class="form-control" />
+                            <label class="form-label" for="form1">Search</label>
+                        </div>
+                        <button type="button" class="btn btn-primary">
+                            <i class="fas fa-search">Search</i>
+                        </button>
+                        </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
