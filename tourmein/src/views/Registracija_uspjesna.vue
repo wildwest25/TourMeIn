18680e372fc20e97e2e3a60ef7f63f5f737b7fea
Luka <wildwest25@hotmail.com>
2021-03-20 21:57:02 +0100

    Napravljena i dotjerana stranica potvrde registracije

--- tourmein/src/views/Registracija_uspjesna.vue
+++ tourmein/src/views/Registracija_uspjesna.vue
@@ -1,8 +1,53 @@
 <template>
-    <div> 
-         <h1>Registration successful</h1>
+    <div class="registration_successful"> 
+
+         <img
+					src="@/assets/explore.jpg"
+					alt="explore the world smjer"
+					height= 50%
+					width= 100%
+					loading="lazy"
+				/>
+
+         <h1> You have successfully registered! </h1>
        
-        <p>Your registration was successful, please check your e-mail for verification link to complete your registration process</p>
+        <p>Your registration was successful, please check your e-mail for verification link to complete your registration process.</p>
+
+        <div class="verify">
+            <h2> 1. Verify your Email </h2>
+
+            <p>To ensure you're a legitimate user and not a robot, Email with verification link has been sent.</p> 
+            <p>If you didn’t get any mail please click resend.</p>
+
+        </div>
+
+        <div class="complete_profile">
+
+        <h2> 2. Complete your profile </h2>
+
+            <p>With your account confirmed, it's time to complete your profile.</p>
+            <p>We've found that adding a profile photo and other information will increase your chances of connecting and touring TourMeIn users by 80%.</p>
+
+        </div>
+
+        <div class="start_touring">
+            
+            <h2> 3. Start touring with us </h2>
+
+            <p> If you're a tourist - use our application to connect with guides and learn something new about places you visit. </p>
+            <p> If you're a guide - connect with tourists in your town, be polite, share your knowledge and make some money. </p>
+
+
+            <img
+					src="@/assets/Logo-01.png"
+					height="60"
+					alt="logo aplikacije"
+					loading="lazy"
+				/>
+        
+        </div>
+
+                <div> TourMeIn</div>
         
     </div>
 </template>
