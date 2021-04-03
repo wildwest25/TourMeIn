1aa1b3cdb9610f068f5c1b2012b4ba673c1e4cf5
mathamer <matija.hamer@student.unipu.hr>
2021-04-03 15:14:24 +0200

    Notifications bugfix

--- tourmein/src/components/Notificationcard.vue
+++ tourmein/src/components/Notificationcard.vue
@@ -13,10 +13,18 @@
 			<div class="col-md" v-if="info.accepted === null">
 				<div class="card-body p-0">{{ info.name }} wants you up as a guide</div>
 				<button type="buttonsuccess" id="successbutton" @click="accept" class="btn btn-primary">
-					<img class="card-img-top offset-1" style="width: 1.2rem; margin-right: 4px;" src="@/assets/success.png" />
+					<img
+						class="card-img-top offset-1"
+						style="width: 1.2rem; margin-right: 4px;"
+						src="@/assets/success.png"
+					/>
 				</button>
-				<button type="buttoncancel" id="cancelbutton" @click="cancelGuide" class="btn btn-primary">
-					<img class="card-img-top offset-1" style="width: 1.2rem margin-right: 4px;" src="@/assets/alert close.png" />
+				<button type="buttoncancel" id="cancelbutton" @click="deny" class="btn btn-primary">
+					<img
+						class="card-img-top offset-1"
+						style="width: 1.2rem margin-right: 4px;"
+						src="@/assets/alert close.png"
+					/>
 				</button>
 			</div>
 			<div class="col-md-auto" v-if="info.accepted === true">
@@ -70,22 +78,22 @@
 				</div>
 			</div>
 			<div v-if="info.accepted === true">
-                <div class="col-md-auto">
-                    <div class="card-body p-0">
-                        <img
-                            class="card-img-top offset-1"
-                            style="width: 2rem; margin-top:10px; position:absolute;"
-                            src="@/assets/success.png"
-                        />
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body p-0">
-                        Guide {{ info.guidename }} Marko Markić accepted your request. You can now ask each
-                        other a message from the top of the further agreement.
-                    </div>
-                </div>
-            </div>
+				<div class="col-md-auto">
+					<div class="card-body p-0">
+						<img
+							class="card-img-top offset-1"
+							style="width: 2rem; margin-top:10px; position:absolute;"
+							src="@/assets/success.png"
+						/>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="card-body p-0">
+						Guide {{ info.guidename }} Marko Markić accepted your request. You can now ask each
+						other a message from the top of the further agreement.
+					</div>
+				</div>
+			</div>
 			<div v-if="info.accepted === false">
 				<div class="col-md-auto">
 					<div class="card-body p-0">
@@ -98,6 +106,9 @@
 				</div>
 				<div class="col-md">
 					<div class="card-body p-0">Guide {{ info.guidename }} turned you down</div>
+					<button type="button" id="button" @click="cancel" class="btn btn-primary">
+						<div id="btn">Ok</div>
+					</button>
 				</div>
 			</div>
 		</div>
@@ -124,7 +135,7 @@ export default {
 					doc.ref.delete();
 					store.tourInProgress = null;
 				});
-				alert('Tour has been canceled!');
+				alert('Tour has been canceled or removed!');
 				window.location.reload();
 			});
 		},
@@ -147,19 +158,52 @@ export default {
 					querySnapshot.forEach((doc) => {
 						console.log(doc.id, ' => ', doc.data());
 
+						const data = doc.data();
 						this.id = doc.id;
 
-						db.collection('tour')
-							.doc(this.id)
-							.update({
-								accepted: true,
-							})
-							.then(() => {
-								console.log('spremljeno, doc');
-							})
-							.catch((e) => {
-								console.error(e);
-							});
+						if (this.info.name === data.name) {
+							db.collection('tour')
+								.doc(this.id)
+								.update({
+									accepted: true,
+								})
+								.then(() => {
+									alert(data.name + ' has been accepted!');
+									window.location.reload();
+								})
+								.catch((e) => {
+									console.error(e);
+								});
+						}
+					});
+				});
+		},
+		deny() {
+			db.collection('tour')
+				.where('guide', '==', store.currentUser)
+				.get()
+				.then((querySnapshot) => {
+					querySnapshot.forEach((doc) => {
+						console.log(doc.id, ' => ', doc.data());
+
+						const data = doc.data();
+						this.id = doc.id;
+
+						console.log(this.info.name, ' => ', data.name);
+						if (this.info.name === data.name) {
+							db.collection('tour')
+								.doc(this.id)
+								.update({
+									accepted: false,
+								})
+								.then(() => {
+									alert(data.name + ' has been denied!');
+									window.location.reload();
+								})
+								.catch((e) => {
+									console.error(e);
+								});
+						}
 					});
 				});
 		},
