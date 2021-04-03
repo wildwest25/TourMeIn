d36f5b6d914e3e6a9bfc8807994c821f20a01856
mathamer <matija.hamer@student.unipu.hr>
2021-04-03 22:22:10 +0200

    TalkJS test

--- tourmein/src/views/Msg_preview.vue
+++ tourmein/src/views/Msg_preview.vue
@@ -9,12 +9,66 @@
 					<div id="msgcard">
 						<user-card id="msgucard" v-for="card in filteredCards" :key="card.url" :info="card" />
 					</div>
+					<div id="talkjs-container" style="width: 90%; margin: 30px; height: 500px">
+						<i>Loading chat...</i>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
 <script>
+(function(t, a, l, k, j, s) {
+	s = a.createElement('script');
+	s.async = 1;
+	s.src = 'https://cdn.talkjs.com/talk.js';
+	a.head.appendChild(s);
+	k = t.Promise;
+	t.Talk = {
+		v: 3,
+		ready: {
+			then: function(f) {
+				if (k)
+					return new k(function(r, e) {
+						l.push([f, r, e]);
+					});
+				l.push([f]);
+			},
+			catch: function() {
+				return k && new k();
+			},
+			c: l,
+		},
+	};
+})(window, document, []);
+
+Talk.ready.then(function() {
+	var me = new Talk.User({
+		id: '123456',
+		name: 'Alice',
+		email: 'alice@example.com',
+		photoUrl: 'https://demo.talkjs.com/img/alice.jpg',
+		welcomeMessage: 'Hey there! How are you? :-)',
+	});
+	window.talkSession = new Talk.Session({
+		appId: 'tcSgqmrC',
+		me: me,
+	});
+	var other = new Talk.User({
+		id: '654321',
+		name: 'Sebastian',
+		email: 'Sebastian@example.com',
+		photoUrl: 'https://demo.talkjs.com/img/sebastian.jpg',
+		welcomeMessage: 'Hey, how can I help?',
+	});
+
+	var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other));
+	conversation.setParticipant(me);
+	conversation.setParticipant(other);
+
+	var inbox = talkSession.createInbox({ selected: conversation });
+	inbox.mount(document.getElementById('talkjs-container'));
+});
 // @ is an alias to /src
 import UserCard from '@/components/UserMessage.vue';
 import store from '@/store';
@@ -33,7 +87,6 @@ export default {
 		this.getMessages();
 	},
 	methods: {
-
 		getMessages() {
 			console.log('firebase dohvat...');
 
